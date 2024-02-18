b1bfba3cac1ae04fb71a948d041d7d7789a8cc2d
NikhilKr2108 <Nikhil2162@outlook.com>
2024-02-18 16:45:18 +0530

    second project

--- Project/weatherApp/main.py
+++ Project/weatherApp/main.py
@@ -2,9 +2,15 @@ import requests
 import json
 import pyttsx3      #import OS Module
 
+# input command
 city = input("Enter your city: ")
+
+# https://www.weatherapi.com weather is according to this website
 url  = f"https://api.weatherapi.com/v1/current.json?key=04d89cdeeb7f4d4a9b7104136241802&q={city}"
+
+# Request from the weather api website
 r    =  requests.get(url)
+
 # Making a speak function what we need to told just write in speak argument
 def speak(text):
     engine = pyttsx3.init()
@@ -12,7 +18,12 @@ def speak(text):
     engine.runAndWait()
 
 # print(r.text)
+# data read by jsom module
 weatherDetails = json.loads(r.text)
+
+# Data arange by in these variables
 w   =   weatherDetails["current"]["temp_f"]
 t   =   weatherDetails["current"]["last_updated"]
+
+# using robospeaker function for speake a temprature
 speak(f"{t} city is {city} and the temprature in faranhite is {w} degrees")
